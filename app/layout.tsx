cf9ac9a996e9f6d722f54aaf973375a4926b737b
Ali <ali.hussein.pre@gmail.com>
2024-07-06 16:06:01 +0200

    feat: update content and add Aliytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export default function Layout({ children }: { children: ReactNode }) {
       className={inter.className + " scroll-smooth antialiased"}
       suppressHydrationWarning
     >
+      <head>
+        <script
+          defer
+          src="https://aliytics.netlify.app/script.js"
+          data-website-id="176aa3d6-7cb7-4bef-af6a-644d42b42833"
+        ></script>
+      </head>
       <body>
         <RootProvider>{children}</RootProvider>
       </body>
