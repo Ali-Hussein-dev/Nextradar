189252d18c6251f6098ab04379c9c1fbf677660f
Ali <ali.hussein.pre@gmail.com>
2024-07-29 11:20:49 +0200

    feat: add two new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,51 @@
 import { Button } from "@/components/button"
 import Link from "next/link"
 import { Metadata } from "next"
+import { Faqs } from "@/components/faqs"
 
 export const metadata: Metadata = {
   title: "NextRadar",
   description: "Curated Next.js Tools & Resources for focused devs",
 }
+const Footer = () => (
+  <footer className="text-center py-4 border-t text-sm dark:text-zinc-500">
+    <p>
+      Built by{" "}
+      <a
+        href="https://x.com/alihussein_20"
+        className="text-sky-600 hover:underline font-semibold"
+      >
+        Ali H
+      </a>
+    </p>
+  </footer>
+)
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center text-center animate-in">
-        <h1 className="mb-4 text-2xl font-extrabold md:text-4xl lg:text-5xl tracking-tighter text-center max-w-2xl text-balance mx-auto">
-          Curated Next.js Tools & Resources for focused devs
-        </h1>
-        <p className="text-lg text-center max-w-2xl mx-auto dark:text-zinc-400">
-          Keeping up with Next.js ecosystem can be overwhelming. We filter the
-          noise, delivering only what matters.
+    <main className="min-h-screen ">
+      <div className="h-[80vh] flex-col-center ">
+        <div className="flex flex-col justify-center text-center animate-in">
+          <h1 className="mb-4 text-2xl font-extrabold md:text-4xl lg:text-5xl tracking-tight text-center max-w-2xl text-balance mx-auto">
+            Curated Next.js Tools & Resources for focused devs
+          </h1>
+          <p className="text-lg md:text-xl text-center max-w-2xl mx-auto dark:text-zinc-400">
+            Keeping up with Next.js ecosystem can be overwhelming. We filter the
+            noise, delivering only what matters.
+          </p>
+          <Button
+            asChild
+            className="mt-4 max-w-[13rem] font-semibold w-full mx-auto"
+          >
+            <Link href="/docs/latest">Discover</Link>
+          </Button>
+        </div>
+        <p className="pt-2">
+          This site is not endorsed by or affiliated with Vercel.
         </p>
-        <Button
-          asChild
-          className="mt-4 max-w-[13rem] font-semibold w-full mx-auto"
-        >
-          <Link href="/docs/latest">Discover</Link>
-        </Button>
       </div>
+      <Faqs />
+      <Footer />
     </main>
   )
 }
---
+++ components/faqs.tsx
@@ -0,0 +1,49 @@
+const faqs = [
+  {
+    question: "What NextRadar is not?",
+    answer:
+      "It is not an alterantive to YT or search engines, but a place to navigate the Next.js ecosystem.",
+  },
+  {
+    question: "How do I evaluate a resource/tool?",
+    answer:
+      "When evaluating a resource, I consider several factors such as the author's credibility, its popularity, and my personal experience from over two years of working with Next.js.",
+  },
+  {
+    question: "It's free, who pay for that?",
+    answer:
+      "No one, it doesn't take much time to maintain at the moment, I may add sponsored resources in the future with clear.",
+  },
+]
+
+//======================================
+export const Faqs = () => {
+  return (
+    <section className="mx-auto max-w-2xl py-8 md:pb-24">
+      <h2 className="text-xl sm:text-2xl md:text-3xl mb-4 font-bold tracking-tighter text-center">
+        Some questions you might have!
+      </h2>
+      <div className="space-y-4">
+        {faqs.map((faq, i) => (
+          <div
+            key={i}
+            className="border border-dashed rounded dark:border-zinc-700 p-4"
+          >
+            <h3 className="font-semibold text-lg">{faq.question}</h3>
+            <p className="dark:text-zinc-500">{faq.answer}</p>
+          </div>
+        ))}
+      </div>
+      <p className="text-center max-w-lg mx-auto dark:text-zinc-400 pt-6">
+        Haven{"'"}t I answered all your questions? Feel free to reach out on{" "}
+        <a
+          href="https://twitter.com/alihusnain_20"
+          className="text-sky-600 hover:underline font-semibold"
+        >
+          X
+        </a>{" "}
+        I will be glad to response.
+      </p>
+    </section>
+  )
+}
