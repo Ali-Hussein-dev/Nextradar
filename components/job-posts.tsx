c09db55e61c1c09c6d8d7780482478f6837c9ba3
Ali <ali.hussein.pre@gmail.com>
2024-07-29 20:43:37 +0200

    feat: change the layout to 2 cols

--- components/job-posts.tsx
+++ components/job-posts.tsx
@@ -6,14 +6,14 @@ export const JobPosts = async () => {
   const list = await getJobPosts()
   return (
     <>
-      <section className="max-w-2xl mx-auto">
+      <section className="max-w-5xl mx-auto">
         <h2 className="mb-4 text-center text-lg">
           Awesome{" "}
           <span className="bg-orange-800/60 px-0.5">job opportunities</span> for
           professional Nextjs devs{" "}
           <span className="bg-teal-800/70 px-0.5">everyday</span>.
         </h2>
-        <div className="grid gap-5">
+        <div className="grid gap-5 md:grid-cols-2">
           {list.map((job, i) => (
             <JobCard key={i} {...job} />
           ))}
