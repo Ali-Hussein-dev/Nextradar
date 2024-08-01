b8e6efff39a30680a91794f9ac96eead63d41136
Ali <ali.hussein.pre@gmail.com>
2024-08-01 16:55:52 +0200

    fix: prevent overriding className

--- components/ui/card-wrapper.tsx
+++ components/ui/card-wrapper.tsx
@@ -8,11 +8,11 @@ export const CardWrapper = ({
   children: React.ReactNode
 } & React.HTMLAttributes<HTMLDivElement>) => (
   <div
+    {...rest}
     className={cn(
-      "rounded-md px-6 pb-3 pt-6 flex-col-center w-full dark:bg-zinc-800/20 bg-white shadow",
+      "rounded-md px-6 pb-3 pt-6 flex flex-col justify-center w-full dark:bg-zinc-800/20 bg-white shadow",
       rest.className
     )}
-    {...rest}
   >
     {children}
   </div>
