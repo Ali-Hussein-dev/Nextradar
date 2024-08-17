baea6e11e602e566c507634ed5b6dc5950be647d
Ali <ali.hussein.pre@gmail.com>
2024-08-17 15:10:33 +0200

    feat: create markdown comp + utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import Slugger from 'github-slugger';
+import { TypedObject } from 'sanity';
 
 const slugger = new Slugger();
 
@@ -10,4 +11,19 @@ export function cn(...inputs: ClassValue[]) {
 
 export function slugify(text: string) {
     return slugger.slug(text);
+}
+
+
+export function blocksToText(blocks: TypedObject[], opts = {}) {
+    const defaults = { nonTextBehavior: 'remove' }
+    const options = Object.assign({}, defaults, opts)
+    return blocks
+        .map(block => {
+            if (block._type !== 'block' || !block.children) {
+                return options.nonTextBehavior === 'remove' ? '' : `[${block._type} block]`
+            }
+
+            return block.children.map(child => child.text).join('')
+        })
+        .join('\n\n')
 }
