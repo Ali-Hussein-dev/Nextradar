2aacd6acad6fcdbf5d0bf7ffa43cabe5cedb2c51
Ali <ali.hussein.pre@gmail.com>
2024-08-17 15:17:04 +0200

    fix: Fix type error in utils.ts file

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,8 +22,8 @@ export function blocksToText(blocks: TypedObject[], opts = {}) {
             if (block._type !== 'block' || !block.children) {
                 return options.nonTextBehavior === 'remove' ? '' : `[${block._type} block]`
             }
-
-            return block.children.map(child => child.text).join('')
+            // @ts-expect-error - children is an array
+            return block.children.map((child: { text: any; }) => child.text).join('')
         })
         .join('\n\n')
 }
