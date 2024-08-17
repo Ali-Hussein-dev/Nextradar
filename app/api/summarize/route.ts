3b4ab56c6bff36023870db50ae1fb7d2080f2f5f
Ali <ali.hussein.pre@gmail.com>
2024-08-17 15:11:44 +0200

    feat: Add component for summarizing job posts & api

---
+++ app/api/summarize/route.ts
@@ -0,0 +1,21 @@
+import { streamText } from 'ai';
+import { openai } from '@ai-sdk/openai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const summarizePrompt = `Summarize the following job post for job seekers and follow these guidlines
+- Max 150 words
+- Divided it into 2 sections: Requirments & Benefits and use headings for each section
+- Include any additional important detail in the bottom of the post under the heading "Additional Details"
+- Use markdown format and make it readable as possible as you can \n\n`;
+
+export async function POST(req: Request) {
+    const { prompt: JobPost }: { prompt: string } = await req.json();
+    const result = await streamText({
+        model: openai('gpt-3.5-turbo-0125'),
+        prompt: `${summarizePrompt} ${JobPost}`,
+    });
+
+    return result.toDataStreamResponse();
+}
