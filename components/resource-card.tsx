58699261c28874fadbdf75a577d1acc54da54298
Ali <ali.hussein.pre@gmail.com>
2024-08-25 14:48:08 +0200

    feat: remove desc on mobile

--- components/resource-card.tsx
+++ components/resource-card.tsx
@@ -1,6 +1,6 @@
 import { FaExternalLinkAlt } from "react-icons/fa"
 import { Button } from "@/components/button"
-import { slugify } from "@/lib/utils"
+import { cn, slugify } from "@/lib/utils"
 import { YtDialog } from "@/components/yt-dialog"
 import { CardWrapper } from "@/components/ui/card-wrapper"
 
@@ -28,9 +28,16 @@ export const ResourceCard = ({
           <h3 className="text-xl font-extrabold mt-0 mb-1 text-pretty scroll-m-20 md:text-xl dark:text-zinc-300/90">
             {name}
           </h3>
-          <p className="mb-1 dark:text-zinc-500 text-pretty line-clamp-3 text-zinc-700">
+
+          <p
+            className={cn(
+              "mb-1 dark:text-zinc-500 text-pretty line-clamp-3 text-zinc-700",
+              src && "hidden md:block"
+            )}
+          >
             {description}
           </p>
+
           {/* --------------------------------Bottom */}
           <div className="flex-row-between gap-2 w-full pt-1">
             <span className="dark:text-zinc-500 text-zinc-400">{author}</span>
