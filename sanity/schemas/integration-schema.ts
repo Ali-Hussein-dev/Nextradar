74d430b20fd93aee1cf276907db6c40cc78d9edb
Ali <ali.hussein.pre@gmail.com>
2024-09-26 16:11:14 +0200

    feat: update schema for integration

--- sanity/schemas/integration-schema.ts
+++ sanity/schemas/integration-schema.ts
@@ -1,51 +1,56 @@
 const IntegrationSchema = {
-    name: 'integration',
-    title: 'Integration',
-    type: 'document',
-    fields: [
-        {
-            name: 'name',
-            title: 'Name',
-            type: 'string',
-        },
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'text',
-        },
-        {
-            name: 'category',
-            title: 'Category',
-            type: 'object',
-            fields: [{ name: 'id', type: 'number', title: 'cateogry ID' },
-            { name: 'name', type: 'string', title: 'Category name' },]
-            // 'reference' is a type that allows us to reference another document
-            // to: { type: 'string' },
-        },
-        {
-            name: 'logoUrl',
-            title: 'Logo URL',
-            type: 'url',
-        },
-        {
-            name: 'url',
-            title: 'URL',
-            type: 'url',
-            initialValue: 'https://',
-        },
-        {
-            name: 'paid',
-            title: 'Is Paid?',
-            type: 'boolean',
-            intialValue: false,
-        },
-        {
-            name: 'sponsored',
-            title: 'Is Sponsored?',
-            type: 'boolean',
-            intialValue: false,
-        },
-    ],
+  name: "integration",
+  title: "Integration",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "object",
+      fields: [
+        { name: "id", type: "number", title: "cateogry ID" },
+        { name: "name", type: "string", title: "Category name" },
+      ],
+      // 'reference' is a type that allows us to reference another document
+      // to: { type: 'string' },
+    },
+    {
+      name: "logoUrl",
+      title: "Logo URL",
+      type: "url",
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+      initialValue: "https://",
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "string" }],
+      options: {
+        layout: "tags",
+      },
+    },
+    {
+      name: "sponsored",
+      title: "Is Sponsored?",
+      type: "boolean",
+      intialValue: false,
+    },
+  ],
 }
 
 export default IntegrationSchema
