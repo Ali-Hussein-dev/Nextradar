9fecf9b173e30993a2ca20afecd5d385893fe930
Ali <ali.hussein.pre@gmail.com>
2024-10-26 15:18:00 +0200

    refactor: delete docs folder & fuma configs files

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-import { createMDX } from 'fumadocs-mdx/next';
-
-const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
 };
 
-export default withMDX(config);
+export default config;
