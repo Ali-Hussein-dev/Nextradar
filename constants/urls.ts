8b52d53ef9363f9a831e138e125d6cb20c8228f3
Ali <ali.hussein.pre@gmail.com>
2024-10-27 11:22:03 +0100

    update constants

--- constants/urls.ts
+++ constants/urls.ts
@@ -8,9 +8,18 @@ export const urls = {
     sponsor: "/content/sponsor",
     templates: "/content/templates",
     osProjects: "/content/real-world-apps",
+    // Integrations
+    hosting: "/content/hosting",
+    headlessCms: "/content/headless-cms",
+    baas: "/content/baas",
+    db: "/content/db",
+    commerce: "/content/commerce",
+    // testing: "/content/testing",
     jobs: "/jobs",
     jobsExpired: "/jobs/expired",
+    // legal pages
     terms: "/legal/tos",
     privacy: "/legal/privacy",
+    // social media
     twitter: "https://twitter.com/ali_hussein_20",
 }
