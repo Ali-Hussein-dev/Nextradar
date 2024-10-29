537748e5d357d828d50ace2f8b5b005a8df58ccf
Ali <ali.hussein.pre@gmail.com>
2024-10-29 07:23:02 +0100

    chore: update private page

--- app/0/copy-sources/page.tsx
+++ app/0/copy-sources/page.tsx
@@ -8,29 +8,33 @@ export default async function CopySources() {
     fields: "name, description, type, href, src, author",
     length: 15,
   })) as any[]
+  const sourceType = "youtube"
   return (
     <div className="pt-10">
       <pre className="max-w-2xl rounded border border-gray-800 p-2 space-y-4 mx-auto">
         <p>Total: {sources.length}</p>
         {sources
-          .filter((o) => o.type === "article")
+          .filter((o) => o.type === sourceType)
           .map((source, i) => (
             <div key={i} className="space-y-3 border p-3">
-              <span className="">{i + 1}</span>
+              <span className="text-xl">
+                {i + 1} - {sourceType === "youtube" ? "▶️" : "📄"}
+              </span>
               <div className=" whitespace-pre-wrap">{source.name}</div>
               {source.descrption}
-              <div className="">
+              <div>
                 <p className="whitespace-pre-wrap">{source.description}</p>
               </div>
               Author: {source.author}
               <div className="flex-row-start gap-3">
-                <CopyButton text={"### " + source.name} />
-                <CopyButton text={source.description} label="copy desc" />
-                <CopyButton label="copy href" text={source.href} />
                 <CopyButton
-                  label="copy author"
-                  text={"Author: " + source.author}
+                  text={`${source.name} ${source.description} - ${source.author}`}
+                  label="Copy all"
                 />
+                <CopyButton label="copy HREF" text={source.href} />
+                {source.src && (
+                  <CopyButton label="copy Embed src" text={source.src} />
+                )}
               </div>
             </div>
           ))}
