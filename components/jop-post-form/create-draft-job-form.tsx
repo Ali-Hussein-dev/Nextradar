2709bdc4761c0e0f5a409cc5fed38462be45f957
Ali <ali.hussein.pre@gmail.com>
2024-11-08 13:19:43 +0100

    feat: refactor job form fields and add workplace type selection

--- components/jop-post-form/create-draft-job-form.tsx
+++ components/jop-post-form/create-draft-job-form.tsx
@@ -3,7 +3,6 @@ import { ControllerRenderProps, useForm } from "react-hook-form"
 import {
   RenderFormElement,
   FormFieldElement,
-  StaticFormElement,
   FieldsElementsList,
 } from "@/components/render-form-element"
 import { z } from "zod"
@@ -40,166 +39,7 @@ const benefits = [
   "No politics at work",
   "We hire old (and young)",
 ]
-const fields: FieldsElementsList = [
-  {
-    variant: "H2",
-    static: true,
-    content: "Job Details",
-    name: "job-details",
-  },
-  [
-    {
-      variant: "Input",
-      name: "jobTitle",
-      placeholder: "e.g Senior Frontend Developer",
-      label: "Job title",
-      required: true,
-    },
-    {
-      variant: "Input",
-      name: "branch",
-      placeholder: "e.g Software Development",
-      label: "Branch",
-    },
-  ],
-  {
-    variant: "Select",
-    options: [
-      { value: "Full-time", label: "Full-time" },
-      { value: "Part-time", label: "Part-time" },
-      { value: "Freelance", label: "Freelance" },
-      { value: "Contract", label: "Contract" },
-      { value: "Internship", label: "Internship" },
-    ],
-    name: "contractType",
-    label: "Select contract type",
-    placeholder: "e.g Full-time",
-    defaultValue: "Full-time",
-  },
-  {
-    variant: "Input",
-    name: "applyUrl",
-    placeholder: "https://acme.com/xxx or email",
-    label: "Application URL",
-    type: "string",
-    required: true,
-  },
-  [
-    {
-      variant: "Input",
-      type: "number",
-      name: "salary.minimum",
-      label: "Minimum salary per year",
-      placeholder: "e.g 50,000",
-      step: 1000,
-    },
-    {
-      variant: "Input",
-      type: "number",
-      name: "salary.maximum",
-      label: "Minimum salary per year",
-      placeholder: "e.g 100,000",
-      step: 1000,
-    },
-  ],
-  {
-    variant: "Select",
-    options: [
-      { value: "$", label: "USD" },
-      { value: "€", label: "EUR" },
-      { value: "£", label: "GBP" },
-    ],
-    // defaultValue: "USD",
-    name: "salary.currency",
-    label: "Select currency",
-    placeholder: "e.g USD",
-  },
-  {
-    variant: "MultiSelect",
-    options: [
-      { label: "Africa", value: "Africa" },
-      { label: "Asia", value: "Asia" },
-      { label: "Europe", value: "Europe" },
-      { label: "Australia", value: "Australia" },
-      { label: "North America", value: "North America" },
-      { label: "South America", value: "South America" },
-    ],
-    name: "timeZone",
-    placeholder: "Select time zone",
-  },
-  {
-    name: "benefits",
-    variant: "MultiSelect",
-    placeholder: "Select job benefits",
-    options: benefits.map((o) => ({ value: o, label: o })),
-  },
-  {
-    variant: "Switch",
-    name: "isReactjsOnly",
-    label: "Reactjs only job",
-    description: "Check this if the job is only for Reactjs developers",
-  },
-  {
-    variant: "Textarea",
-    name: "longDescription",
-    label: "Job post body",
-    description: "",
-  },
-  // {
-  //   variant: "Separator",
-  //   static: true,
-  //   decorative: true,
-  //   name: "separator-1",
-  // },
-  {
-    variant: "H2",
-    static: true,
-    content: "Company Details",
-    name: "company-details",
-  },
-  [
-    {
-      variant: "Input",
-      name: "company.name",
-      placeholder: "Acme Inc",
-      label: "Company name",
-    },
-    {
-      variant: "Input",
-      name: "company.website",
-      placeholder: "https://acme.com",
-      label: "Company website",
-      type: "url",
-    },
-  ],
-  [
-    {
-      variant: "Input",
-      name: "company.recruiterName",
-      placeholder: "e.g John Doe",
-      label: "Recruiter name",
-    },
-    {
-      variant: "Input",
-      name: "company.recruiterEmail",
-      placeholder: "e.g jd@acme.com",
-      label: "Recruiter email",
-      type: "email",
-    },
-  ],
-  {
-    variant: "Switch",
-    name: "company.isHiringAgency",
-    label: "Hiring agency",
-    description: "Check this if you are a hiring agency",
-  },
-  {
-    variant: "Checkbox",
-    name: "agreement",
-    label: "I agree to the terms and conditions",
-    required: true,
-  },
-]
+
 
 type FormDrafJob = z.infer<typeof DrafJobFormSchema>
 //======================================
@@ -208,27 +48,202 @@ export function CreateDraftJobForm() {
     defaultValues: {
       contractType: "Full-time",
       salary: {
-        // currency: "USD",
+        currency: "USD",
       },
       timeZone: [],
       benefits: [],
       company: {},
     },
-    resolver: zodResolver(DrafJobFormSchema),
+    // resolver: zodResolver(DrafJobFormSchema),
   })
   const { handleSubmit } = form
   const { mutate, status } = useServerActionMutation(handleJobFormSubmission, {
-    mutationKey: ["createDraftJobPost"],
+    mutationKey: ["createDraftJobPost-1"],
   })
   const onSubmit: (data: FormDrafJob) => void = (data) => {
     mutate(data)
   }
+  const fields: FieldsElementsList = [
+    {
+      variant: "H2",
+      static: true,
+      content: "JOB DETAILS",
+      name: "job-details",
+    },
+    [
+      {
+        variant: "Input",
+        name: "jobTitle",
+        placeholder: "e.g Senior Frontend Developer",
+        label: "Job title",
+        // required: true,
+      },
+      {
+        variant: "Input",
+        name: "branch",
+        placeholder: "e.g Software Development",
+        label: "Branch",
+      },
+    ],
+    [
+      {
+        variant: "Select",
+        options: [
+          { value: "Full-time", label: "Full-time" },
+          { value: "Part-time", label: "Part-time" },
+          { value: "Freelance", label: "Freelance" },
+          { value: "Contract", label: "Contract" },
+          { value: "Internship", label: "Internship" },
+        ],
+        name: "contractType",
+        label: "Select contract type",
+        placeholder: "e.g Full-time",
+        defaultValue: "Full-time",
+      },
+      {
+        variant: "Select",
+        options: [
+          { value: "Remote", label: "Remote" },
+          { value: "Onsite", label: "Onsite" },
+          { value: "Hybrid", label: "Hybrid" },
+        ],
+        name: "workplaceType",
+        label: "Select workplace",
+        placeholder: "e.g Remote",
+        defaultValue: "Remote",
+      },
+    ],
+    {
+      variant: "Input",
+      name: "applyUrl",
+      placeholder: "https://acme.com/xxx or email",
+      label: "Application URL",
+      type: "string",
+      // required: true,
+    },
+    [
+      {
+        variant: "Input",
+        type: "number",
+        name: "salary.minimum",
+        label: "Minimum salary per year",
+        placeholder: "e.g 50,000",
+        step: 1000,
+      },
+      {
+        variant: "Input",
+        type: "number",
+        name: "salary.maximum",
+        label: "Minimum salary per year",
+        placeholder: "e.g 100,000",
+        step: 1000,
+      },
+      {
+        variant: "Select",
+        options: [
+          { value: "$", label: "USD" },
+          { value: "€", label: "EUR" },
+          { value: "£", label: "GBP" },
+        ],
+        // defaultValue: "USD",
+        name: "salary.currency",
+        label: "Select currency",
+        placeholder: "e.g USD",
+      },
+    ],
+    {
+      variant: "MultiSelect",
+      options: [
+        { label: "Africa", value: "Africa" },
+        { label: "Asia", value: "Asia" },
+        { label: "Europe", value: "Europe" },
+        { label: "Australia", value: "Australia" },
+        { label: "North America", value: "North America" },
+        { label: "South America", value: "South America" },
+      ],
+      name: "timeZone",
+      placeholder: "Select time zone",
+    },
+    {
+      name: "benefits",
+      variant: "MultiSelect",
+      placeholder: "Select job benefits",
+      options: benefits.map((o) => ({ value: o, label: o })),
+    },
+    {
+      variant: "Switch",
+      name: "isReactjsOnly",
+      label: "Reactjs only job",
+      description: "Check this if the job is only for Reactjs developers",
+    },
+    {
+      variant: "RichText",
+      name: "longDescription",
+      value: form.watch("longDescription") ?? "",
+      setValue: form.setValue as (name: string, value: string) => void,
+      placeholder: "",
+    },
+    // {
+    //   variant: "Separator",
+    //   static: true,
+    //   decorative: true,
+    //   name: "separator-1",
+    // },
+    {
+      variant: "H2",
+      static: true,
+      content: "COMPANY DETAILS",
+      name: "company-details",
+    },
+    [
+      {
+        variant: "Input",
+        name: "company.name",
+        placeholder: "Acme Inc",
+        label: "Company name",
+      },
+      {
+        variant: "Input",
+        name: "company.website",
+        placeholder: "https://acme.com",
+        label: "Company website",
+        type: "url",
+      },
+    ],
+    [
+      {
+        variant: "Input",
+        name: "company.recruiterName",
+        placeholder: "e.g John Doe",
+        label: "Recruiter name",
+      },
+      {
+        variant: "Input",
+        name: "company.recruiterEmail",
+        placeholder: "e.g jd@acme.com",
+        label: "Recruiter email",
+        type: "email",
+      },
+    ],
+    {
+      variant: "Switch",
+      name: "company.isHiringAgency",
+      label: "Hiring agency",
+      description: "Check this if you are a hiring agency",
+    },
+    {
+      variant: "Checkbox",
+      name: "agreement",
+      label: "I agree to the terms and conditions",
+      // required: true,
+    },
+  ]
   return (
     <div className="max-w-2xl mx-auto animate-in">
       <Form {...form}>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col gap-3 mx-auto w-full "
+          className="flex flex-col gap-4 mx-auto w-full "
         >
           {fields.map((item, i) => {
             if (item instanceof Array) {
