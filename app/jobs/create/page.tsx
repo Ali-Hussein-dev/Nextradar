00067b2804f77b4267a54c5e15445901e0d7901d
Ali <ali.hussein.pre@gmail.com>
2024-11-08 16:09:12 +0100

    feat: update job post creation page layout and improve headings for clarity

--- app/jobs/create/page.tsx
+++ app/jobs/create/page.tsx
@@ -1,15 +1,38 @@
 import { CreateDraftJobForm } from "@/components/jop-post-form/create-draft-job-form"
 
+// const SideBar = () => {
+//   return (
+//     <div className="md:col-span-2 p-2">
+//       <h1 className="text-2xl font-extrabold mb-4 mt-6 text-center text-pretty animate-in dark:text-zinc-100">
+//         Hire Experineced Developers in Reactjs & Nextjs framework
+//       </h1>
+//     </div>
+//   )
+// }
+
 //======================================
 export default function CreateJobPostPage() {
   return (
-    <div className="max-w-4xl mx-auto p-1.5 sm:p-4 md:p-8">
-      
-      <div className="p-3 md:p-6 rounded-sm md:shadow-inner border border-dashed">
-        <h1 className="text-2xl md:text-3xl font-bold mb-4 mt-8 text-center text-pretty animate-in">
-          Hire Experineced Developers in Reactjs & Nextjs framework
-        </h1>
-        <CreateDraftJobForm />
+    <div className="max-w-4xl mx-auto p-1.5 sm:p-4 w-full">
+      <div className=" md:grid-cols-8 gap-4">
+        <div className="p-3 md:p-6 rounded-sm md:shadow-inner border border-dashed md:col-span-6">
+          <h1 className="text-2xl mb-4 mt-6 text-center text-pretty animate-in dark:text-zinc-200 font-bold">
+            Hire qualified Reactjs & Nextjs developers
+          </h1>
+          <CreateDraftJobForm />
+        </div>
+        <div className="md:col-span-2 p-2">
+          {/* <div>
+            <div className="flex-row-center gap-3">
+              {["str", "str"].map((o, i) => (
+                <div
+                  key={i}
+                  className="size-12 dark:bg-zinc-800 rounded-sm"
+                ></div>
+              ))}
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   )
@@ -17,5 +40,5 @@ export default function CreateJobPostPage() {
 
 export const metadata = {
   title: "Create Job Post",
-  description: "Hire experienced developers in Reactjs & Nextjs framework",
+  description: "Hire qualified Reactjs & Nextjs developers",
 }
--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,23 +1,28 @@
 import { JobPostsSection } from "@/components/sections/job-posts-section"
+import { Button } from "@/components/ui/button"
+import { urls } from "@/constants/urls"
+import Link from "next/link"
 import * as React from "react"
 
 //======================================
 export default function JobsPage() {
   return (
     <div className="max-w-5xl mx-auto w-full">
-      <div className="p-2 max-w-[45rem] mx-auto mb-8 pt-6 pb-4">
-        <div className="px-2 mx-auto">
-          <h1 className="mb-3 text-xl sm:text-2xl font-bold text-center text-pretty">
+      <div className="max-w-3xl mx-auto mb-4 pt-6">
+        <div className="pb-2">
+          <h1 className="mb-3 text-xl font-extrabold text-center text-pretty">
             Top Jobs Opportunities for Specialized Developers
           </h1>
-          <p className="dark:text-zinc-500 tracking-wide text-base text-center ">
+          <p className="dark:text-zinc-500 tracking-wide text-base text-center  text-pretty">
             Get hired by companies who are looking for your skills and
-            experience in <br /> Next.js and React.js
+            experience in Next.js and React.js
           </p>
         </div>
-        {/* <div className="flex-row-end w-full pr-1 pb-1">
-          <Button variant="outline">Post a job</Button>
-        </div> */}
+        <div className="flex-row-end w-full">
+          <Button variant="outline" asChild>
+            <Link href={urls.createJob}>Post a job</Link>
+          </Button>
+        </div>
       </div>
       <React.Suspense>
         <JobPostsSection />
