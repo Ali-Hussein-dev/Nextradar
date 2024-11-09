c68938b825e3452b2874c82bb99b346b337614c8
Ali <ali.hussein.pre@gmail.com>
2024-11-09 09:43:41 +0100

    fix: handle window is not defined error

--- app/jobs/create/page.tsx
+++ app/jobs/create/page.tsx
@@ -1,37 +1,45 @@
-import { CreateDraftJobForm } from "@/components/jop-post-form/create-draft-job-form"
+import dynamic from "next/dynamic"
 
-// const SideBar = () => {
-//   return (
-//     <div className="md:col-span-2 p-2">
-//       <h1 className="text-2xl font-extrabold mb-4 mt-6 text-center text-pretty animate-in dark:text-zinc-100">
-//         Hire Experineced Developers in Reactjs & Nextjs framework
-//       </h1>
-//     </div>
-//   )
-// }
+const CreateDraftJobForm = dynamic(
+  () =>
+    import("@/components/jop-post-form/create-draft-job-form").then(
+      (mod) => mod.CreateDraftJobForm
+    ),
+  { ssr: false }
+)
 
 //======================================
 export default function CreateJobPostPage() {
   return (
-    <div className="max-w-4xl mx-auto p-1.5 sm:p-4 w-full">
-      <div className=" md:grid-cols-8 gap-4">
-        <div className="p-3 md:p-6 rounded-sm md:shadow-inner border border-dashed md:col-span-6">
-          <h1 className="text-2xl mb-4 mt-6 text-center text-pretty animate-in dark:text-zinc-200 font-bold">
-            Hire qualified Reactjs & Nextjs developers
-          </h1>
+    <div className="max-w-6xl mx-auto p-1.5 md:p-4 w-full lg:relative">
+      <div className="grid lg:grid-cols-10 gap-6">
+        <div className="p-3 lg:p-6 rounded-sm border border-dashed lg:col-span-7 lg:w-full">
           <CreateDraftJobForm />
         </div>
-        <div className="md:col-span-2 p-2">
-          {/* <div>
-            <div className="flex-row-center gap-3">
-              {["str", "str"].map((o, i) => (
-                <div
-                  key={i}
-                  className="size-12 dark:bg-zinc-800 rounded-sm"
-                ></div>
-              ))}
+        <div className="lg:col-span-3 animate-in">
+          <div className="lg:sticky lg:top-5">
+            <h1 className="text-xl text-pretty dark:text-zinc-200 font-bold border-x border-t md:px-6 py-7 border-dashed rounded-sm text-center">
+              Spcialized job board <br /> for Reactjs & Nextjs engineers
+            </h1>
+            <div className="prose dark:prose-invert prose-zinc border md:px-6 py-7 border-dashed p-4">
+              <h2 className="text-lg font-medium mb-2">
+                Why List on Nextradar?
+              </h2>
+              <ul className="space-y-3 mt-1">
+                {[
+                  "30-day premium listing visibility",
+                  "Reach Targeted Candidates: Developers who specialize in Next.js and React.js",
+                  "Multi-channel distribution across LinkedIn, Reddit, and specialized tech newsletters",
+                  "Significantly cheaper than LinkedIn's €1400 for the same duration",
+                ].map((str, i) => (
+                  <li key={i} className="">
+                    {/* <TiInputChecked className="size-6" /> */}
+                    {str}
+                  </li>
+                ))}
+              </ul>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
@@ -39,6 +47,6 @@ export default function CreateJobPostPage() {
 }
 
 export const metadata = {
-  title: "Create Job Post",
-  description: "Hire qualified Reactjs & Nextjs developers",
+  title: "Hire Reactjs & Nextjs developers",
+  description: "Hire qualified Reactjs & Nextjs developers remotely",
 }
