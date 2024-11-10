5d4d0e215e3113bb6a15c17826345eb9507a9d25
Ali <ali.hussein.pre@gmail.com>
2024-11-10 11:07:54 +0100

    fix: correct perspective setting for development environment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,12 +4,13 @@ import { apiVersion, dataset, projectId, useCdn } from '@/sanity/env'
 
 const isDev = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
 
+
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn,
-  perspective: !isDev ? 'previewDrafts' : 'published',
+  perspective: isDev ? 'previewDrafts' : 'published',
   token: process.env.SANITY_API_TOKEN,
 }).withConfig({
   useCdn: false
