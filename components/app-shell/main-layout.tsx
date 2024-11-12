df371cdb71a369ff1d4858803673f055bd27ec9d
Ali <ali.hussein.pre@gmail.com>
2024-11-12 14:42:58 +0100

    fix: fix toggling theme

--- components/app-shell/main-layout.tsx
+++ components/app-shell/main-layout.tsx
@@ -1,21 +1,29 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-shell/app-sidebar"
-import { ReactQueryProv } from "@/components/providers"
+import { ReactQueryProv, ThemeProvider } from "@/components/providers"
 import { Footer } from "@/components/shared/footer"
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">
-        <nav className="px-4 py-1">
-          <SidebarTrigger />
-        </nav>
-        <div className="pt-2 pb-8 h-full px-3 md:px-1">
-          <ReactQueryProv>{children}</ReactQueryProv>
-        </div>
-        <Footer />
-      </main>
-    </SidebarProvider>
+    <ThemeProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">
+          <nav className="px-4 py-1">
+            <SidebarTrigger />
+          </nav>
+          <div className="relative">
+            <div className="pt-2 pb-8 h-full px-3 md:px-1">
+              <ReactQueryProv>{children}</ReactQueryProv>
+            </div>
+          </div>
+          <Footer />
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   )
 }
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,10 +1,28 @@
 "use client"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { getQueryClient } from "@/lib/get-query-client"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export const ReactQueryProv = ({ children }: { children: React.ReactNode }) => {
   const queryClient = getQueryClient()
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
   )
 }
