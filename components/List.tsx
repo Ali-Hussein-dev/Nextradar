869f0336ea3d982ba2a743887fd5f63a22f67d84
Ali <ali.hussein.pre@gmail.com>
2024-12-02 14:10:25 +0100

    refactor: remove getSites call

--- components/List.tsx
+++ components/List.tsx
@@ -1,6 +1,6 @@
 import { Repo } from "@/lib/get-repos-github"
 import { RepoCard } from "@/components/repo-card"
-import { getReposList, getSites, RepoCategory } from "@/sanity/lib/getters"
+import { getReposList, RepoCategory } from "@/sanity/lib/getters"
 
 //======================================
 export const ItemsList = async ({
@@ -10,10 +10,7 @@ export const ItemsList = async ({
   category: RepoCategory
   recommended?: boolean
 }) => {
-  const list =
-    category == "Learn"
-      ? await getSites()
-      : ((await getReposList({ recommended, category })) as Repo[])
+  const list = (await getReposList({ recommended, category })) as Repo[]
 
   return (
     <div className="grid lg:grid-cols-2 gap-4 w-full">
