9d5c68638ba0f05914b996272bc03733256650d3
Ali <ali.hussein.pre@gmail.com>
2024-12-05 12:38:05 +0100

    feat(urls): add React.js and Next.js job URLs to constants

--- constants/urls.ts
+++ constants/urls.ts
@@ -16,11 +16,14 @@ export const urls = {
     commerce: "/content/commerce",
     // testing: "/content/testing",
     jobs: "/jobs",
+    reactJobs: "/jobs/reactjs-jobs",
+    nextJobs: "/jobs/nextjs-jobs",
     createJob: "/jobs/create",
     jobsExpired: "/jobs/expired",
     // legal pages
     terms: "/legal/tos",
     privacy: "/legal/privacy",
+    // imprint: "/legal/imprint",
     // social media
     twitter: "https://twitter.com/ali_hussein_20",
 }
