c66924a88a46f89d6e0baadf1e7d3afbcf669830
Ali <ali.hussein.pre@gmail.com>
2024-12-06 10:31:33 +0100

    feat(jobs): enhance job page layout and benefits display logic

--- jobs/components/job-page.tsx
+++ jobs/components/job-page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/button"
 import Link from "next/link"
 import { FaArrowLeft } from "react-icons/fa"
 import { PortableText } from "next-sanity"
-import { TldrJobPost } from "@/jobs/components/tldr-job-post"
+// import { TldrJobPost } from "@/jobs/components/tldr-job-post"
 import { urls } from "@/constants/urls"
 import { IoEarthOutline } from "react-icons/io5"
 import { CiLocationOn } from "react-icons/ci"
@@ -17,6 +17,7 @@ import { PiHouseLine } from "react-icons/pi"
 
 //======================================
 export const FullJobPost = ({ post }: { post: JobPost }) => {
+  const benefits = post?.benefits ?? []
   return (
     <section className="py-6 mx-auto border border-dashed">
       <div className="flex-col-center gap-8 dark:text-zinc-400 text-zinc-700 p-3 py-6 border-b border-dashed">
@@ -27,12 +28,12 @@ export const FullJobPost = ({ post }: { post: JobPost }) => {
         <h1 className="text-2xl md:text-3xl lg:text-4xl dark:text-zinc-200 font-bold text-zinc-900 text-center">
           {post.jobTitle}
         </h1>
-        <div className="flex-row-center gap-5 text-sm">
-          <span className="capitalize flex-row-start gap-2">
+        <div className="flex-row-center gap-5 text-sm flex-wrap">
+          <span className="capitalize flex-row-start gap-1.5">
             <CgFileDocument size="15" />
             {post.contractType}
           </span>
-          <span className="capitalize flex-row-start gap-2">
+          <span className="capitalize flex-row-start gap-1.5">
             <PiHouseLine size="15" />
             {post.workplaceType}
           </span>
@@ -45,15 +46,15 @@ export const FullJobPost = ({ post }: { post: JobPost }) => {
             ""
           )}
           {post?.location && (
-            <div className="flex-row-start gap-2">
+            <div className="capitalize flex-row-start gap-1.5">
               <CiLocationOn size="15" />
               <span>{post.location}</span>
             </div>
           )}
           {post?.salary && post?.salary.range && (
-            <div className="capitalize flex-row-start gap-2">
+            <div className="capitalize flex-row-start gap-1.5">
               <GiMoneyStack size="15" />
-              <div className="flex-row-end gap-1">
+              <div className="flex-row-end gap-0.5">
                 <span>{post.salary?.range}</span>
                 <span className="uppercase">{post.salary.currency}</span>
               </div>
@@ -62,19 +63,25 @@ export const FullJobPost = ({ post }: { post: JobPost }) => {
         </div>
       </div>
       <article className="typography px-3 md:px-5 lg:px-12 pt-3 mb-6 animate-in">
-        <div className="grid lg:grid-cols-4 lg:gap-8 w-full pt-1">
+        <div className="grid lg:grid-cols-6 lg:gap-8 w-full pt-1">
           <div
             className={cn(
-              post?.benefits ? "lg:col-span-3 pr-2" : "lg:col-span-4"
+              benefits.length > 0
+                ? "lg:col-span-4"
+                : "max-w-3xl mx-auto lg:col-span-6"
             )}
           >
-            <TldrJobPost jobPost={post.longDescription!} />
+            {/* <TldrJobPost jobPost={post.longDescription!} /> */}
             <PortableText value={post.longDescription!} />
           </div>
-          {post?.benefits && (
-            <div className="lg:pt-10">
+          {benefits.length > 0 ? (
+            <div className="lg:pt-6 border px-5 border-dashed rounded-sm lg:col-span-2 h-fit pb-6">
               <h2>Perks</h2>
-              <ul>{post.benefits?.map((str) => <li key={str}>{str}</li>)}</ul>
+              <ul>
+                {benefits.map((str) => (
+                  <li key={str}>{str}</li>
+                ))}
+              </ul>
               <Button asChild size="sm" className="w-full">
                 <a href={post.applyUrl}>
                   <span className="mr-1.5">Apply</span>
@@ -86,15 +93,15 @@ export const FullJobPost = ({ post }: { post: JobPost }) => {
                 </a>
               </Button>
             </div>
-          )}
+          ) : null}
         </div>
-        {!post?.benefits && (
-          <Button asChild size="sm" className="w-full">
+        {benefits.length < 1 ? (
+          <Button asChild size="sm" className="w-full mx-auto max-w-3xl">
             <a href={post.applyUrl} className="flex-row-center gap-1">
               Apply <GoArrowUpRight className="" size="18" />
             </a>
           </Button>
-        )}
+        ) : null}
       </article>
       <div className="border-t border-dashed pt-4 flex-row-end w-full pr-4">
         <Button asChild variant={"outline"}>
