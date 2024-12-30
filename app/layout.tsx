fa60d0455cc9630eba7fcf8c86d1f0ca0e4063b3
Ali <ali.hussein.pre@gmail.com>
2024-12-30 14:02:41 +0100

    refactor: remove fumadocs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { AnalyticsProv } from "@/components/analytics-prov"
 import "./global.css"
-import { RootProvider } from "fumadocs-ui/provider"
 import { Poppins } from "next/font/google"
 import type { ReactNode } from "react"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
@@ -19,11 +18,10 @@ export default function Layout({ children }: { children: ReactNode }) {
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <AnalyticsProv />
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
       </head>
       <body>
-        <RootProvider>
-          <NuqsAdapter>{children}</NuqsAdapter>
-        </RootProvider>
+        <NuqsAdapter>{children}</NuqsAdapter>
       </body>
     </html>
   )
