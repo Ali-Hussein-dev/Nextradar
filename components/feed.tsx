fb85d008706a0f0d226ef1b4efb1e8513a0af646
Ali <ali.hussein.pre@gmail.com>
2025-01-04 13:37:12 +0100

    refactor: remove memo

--- components/feed.tsx
+++ components/feed.tsx
@@ -26,67 +26,64 @@ export type FeedCardProps = {
   rel: string
 }
 //======================================
-export const FeedCard = React.memo(
-  ({
-    name,
-    description,
-    author,
-    href,
-    src,
-    sponsored,
-    rel = "nofollow",
-  }: FeedCardProps) => {
-    return (
-      <Card
-        className={`shadow-none ${sponsored ? "dark:border-green-300/30 border-green-300/60 bg-muted/30" : ""}`}
-      >
-        <CardHeader className="flex flex-col md:flex-row items-start h-full sm:items-start md:gap-3 w-full">
-          {/* --------------------------------YT-Embed */}
-          {src && <YtDialog src={src} href={href} />}
-          <div className="w-full pt-2 grow flex-col-start md:h-full gap-1">
-            <CardTitle className="flex-row-between w-full gap-5">
-              <span className="line-clamp-1">{name}</span>
-              {sponsored && (
-                <span className="text-xs text-green-300 font-light">
-                  Sponsored
-                </span>
-              )}
-            </CardTitle>
-            <CardDescription
-              className={cn(
-                "mb-1.5 dark:text-zinc-500 text-pretty line-clamp-2 text-zinc-700",
-                src ? "hidden md:line-clamp-2" : ""
-              )}
-            >
-              {description}
-            </CardDescription>
-          </div>
-        </CardHeader>
-        {/* <CardContent>
+export const FeedCard = ({
+  name,
+  description,
+  author,
+  href,
+  src,
+  sponsored,
+  rel = "nofollow",
+}: FeedCardProps) => {
+  return (
+    <Card
+      className={`shadow-none ${sponsored ? "dark:border-green-300/30 border-green-300/60 bg-muted/30" : ""}`}
+    >
+      <CardHeader className="flex flex-col md:flex-row items-start h-full sm:items-start md:gap-3 w-full">
+        {/* --------------------------------YT-Embed */}
+        {src && <YtDialog src={src} href={href} />}
+        <div className="w-full pt-2 grow flex-col-start md:h-full gap-1">
+          <CardTitle className="flex-row-between w-full gap-5">
+            <span className="line-clamp-1">{name}</span>
+            {sponsored && (
+              <span className="text-xs text-green-300 font-light">
+                Sponsored
+              </span>
+            )}
+          </CardTitle>
+          <CardDescription
+            className={cn(
+              "mb-1.5 dark:text-zinc-500 text-pretty line-clamp-2 text-zinc-700",
+              src ? "hidden md:line-clamp-2" : ""
+            )}
+          >
+            {description}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      {/* <CardContent>
           <p>Card Content</p>
         </CardContent> */}
-        <CardFooter className="flex-row-between gap-2 w-full text-sm pt-0">
-          <span className="text-muted-foreground/80">{author}</span>
-          {!src && (
-            <Button
-              variant="outline"
-              asChild
-              className="gap-1.5 no-underline bg-transparent"
-              size="sm"
-            >
-              <a href={href} target="_blank" rel={rel}>
-                Visit
-                <MdOutlineArrowOutward size="14" />
-              </a>
-            </Button>
-          )}
-        </CardFooter>
-      </Card>
-    )
-  }
-)
+      <CardFooter className="flex-row-between gap-2 w-full text-sm pt-0">
+        <span className="text-muted-foreground/80">{author}</span>
+        {!src && (
+          <Button
+            variant="outline"
+            asChild
+            className="gap-1.5 no-underline bg-transparent"
+            size="sm"
+          >
+            <a href={href} target="_blank" rel={rel}>
+              Visit
+              <MdOutlineArrowOutward size="14" />
+            </a>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  )
+}
 
-FeedCard.displayName = "FeedCard"
 //======================================
 export const Feed = async () => {
   const page = await getSourcesPage({ page: 1, pageSize: 10 })
