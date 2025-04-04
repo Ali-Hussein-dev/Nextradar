87eb42a08cd4eb722350b106ec57dddbf3ab73ac
Ali <ali.hussein.pre@gmail.com>
2025-04-04 19:59:20 +0200

    feat: mix changes to OG route

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,6 +2,7 @@
 import { urls } from "@/constants/urls";
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
+// import localFont from "next/font/local";
 
 export const runtime = "edge";
 
@@ -10,15 +11,15 @@ export async function GET(req: NextRequest) {
   const postTitle = searchParams.get("title");
   const fontData = await fetch(
     new URL(`${urls.siteUrl}/Poppins-Bold.ttf`, import.meta.url)
-  )
-    .then((res) => res.arrayBuffer())
-    .catch(console.error);
+  ).then((res) => res.arrayBuffer());
+  // const fontData = localFont({ src: "./public/Poppins-Bold.ttf" })
 
   return new ImageResponse(
     (
       <div
         tw="h-full w-full flex flex-col items-center text-center justify-center font-bold capitalize"
         style={{
+          // backgroundImage: "url('/public/og-bg.jpg')",
           backgroundImage: `url(${urls.siteUrl}/og-bg.jpg)`,
         }}
       >
@@ -33,9 +34,9 @@ export async function GET(req: NextRequest) {
             fontStyle: "normal",
             color: "white",
             letterSpacing: "0.02em",
-            lineHeight: "86px",
+            lineHeight: "100px",
             whiteSpace: "pre-wrap",
-            maxWidth: "968px",
+            // maxWidth: "1000px",
             textAlign: "center",
           }}
         >
--- constants/urls.ts
+++ constants/urls.ts
@@ -1,38 +1,46 @@
 /**
  * This file contains all the urls used in the application
  */
-const siteUrl= {prod: "https://nextradar.io", dev: "http://localhost:3000"}
+
+const siteUrl = {
+    development: "http://localhost:3000",
+    preview: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+    production: "https://nextradar.io",
+};
+export const devEnv = (process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development") as keyof typeof siteUrl;
+
 export const urls = {
-    siteUrl: process.env.NODE_ENV === "development" ? siteUrl.dev : siteUrl.prod,
-    learn: "/content/learn",
-    tools: "/content/tools",
-    latest: "/content/latest",
-    sponsor: "/content/sponsor",
-    templates: "/content/templates",
-    osProjects: "/content/real-world-apps",
-    // Integrations
-    hosting: "/content/hosting",
-    headlessCms: "/content/headless-cms",
-    baas: "/content/baas",
-    db: "/content/db",
-    commerce: "/content/commerce",
-    // testing: "/content/testing",
-    jobs: "/jobs",
-    reactJobs: "/jobs/reactjs-jobs",
-    nextJobs: "/jobs/nextjs-jobs",
-    createJob: "/jobs/create",
-    jobsExpired: "/jobs/expired",
-    // legal pages
-    terms: "/legal/tos",
-    privacy: "/legal/privacy",
-    // imprint: "/legal/imprint",
-    // social media
-    twitter: "https://twitter.com/ali_hussein_20",
-    sponsorArticle:"https://www.creem.io/payment/prod_X6e6WvvE8HRhnOOkCuvEL",
-    sponsorProduct:"https://www.creem.io/payment/prod_4923J97j2SLxwpgyAlRGfr",
-    chatgptAlternatives: "https://chatgptalternatives.app",
-    indieStarter: "https://indie-starter.dev",
-}
+  siteUrl: siteUrl[devEnv],
+  learn: "/content/learn",
+  tools: "/content/tools",
+  latest: "/content/latest",
+  sponsor: "/content/sponsor",
+  templates: "/content/templates",
+  osProjects: "/content/real-world-apps",
+  // Integrations
+  hosting: "/content/hosting",
+  headlessCms: "/content/headless-cms",
+  baas: "/content/baas",
+  db: "/content/db",
+  commerce: "/content/commerce",
+  // testing: "/content/testing",
+  jobs: "/jobs",
+  reactJobs: "/jobs/reactjs-jobs",
+  nextJobs: "/jobs/nextjs-jobs",
+  createJob: "/jobs/create",
+  jobsExpired: "/jobs/expired",
+  // legal pages
+  terms: "/legal/tos",
+  privacy: "/legal/privacy",
+  // imprint: "/legal/imprint",
+  // social media
+  twitter: "https://twitter.com/ali_hussein_20",
+  sponsorArticle: "https://www.creem.io/payment/prod_X6e6WvvE8HRhnOOkCuvEL",
+  sponsorProduct: "https://www.creem.io/payment/prod_4923J97j2SLxwpgyAlRGfr",
+  chatgptAlternatives: "https://chatgptalternatives.app",
+  indieStarter: "https://indie-starter.dev",
+};
 
-export const urlsSitemap = Object.values(urls).filter(url => !url.startsWith("https://"))
-
+export const urlsSitemap = Object.values(urls).filter(
+  (url) => !url.startsWith("https://")
+);
