10c487d2f785e5f21443267cca796a4160f1d2e2
Ali <ali.hussein.pre@gmail.com>
2025-04-15 14:15:04 +0200

    feat: add picing tiers to integration schema

--- sanity/schemas/integration-schema.ts
+++ sanity/schemas/integration-schema.ts
@@ -1,19 +1,21 @@
-const IntegrationSchema = {
+import { defineField, defineType } from "sanity";
+
+const IntegrationSchema = defineType({
   name: "integration",
   title: "Integration",
   type: "document",
   fields: [
-    {
+    defineField({
       name: "name",
       title: "Name",
       type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "description",
       title: "Description",
       type: "text",
-    },
-    {
+    }),
+    defineField({
       name: "category",
       title: "Category",
       type: "object",
@@ -23,24 +25,65 @@ const IntegrationSchema = {
       ],
       // 'reference' is a type that allows us to reference another document
       // to: { type: 'string' },
-    },
-    {
+    }),
+    defineField({
+      name: "pricing",
+      title: "Pricing Catalog 💰",
+      type: "object",
+      fields: [
+        { name: "pricingUrl", type: "url", title: "Pricing URL" },
+        {
+          name: "currency",
+          type: "string",
+          title: "Currency",
+          initialValue: "$",
+        },
+        {
+          name: "tiers",
+          type: "array",
+          title: "Tiers",
+          of: [
+            {
+              type: "object",
+              fields: [
+                { name: "name", type: "string" },
+                {
+                  name: "monthlyPrice",
+                  type: "number",
+                  title: "Monthly Price -1 for custom price",
+                },
+                {
+                  name: "yearlyPrice",
+                  type: "number",
+                  title: "Yearly Price -1 for custom price",
+                },
+                { name: "isTrial", type: "boolean" },
+                { name: "trialDurationDays", type: "number" },
+              ],
+            },
+          ],
+        },
+      ],
+      // 'reference' is a type that allows us to reference another document
+      // to: { type: 'string' },
+    }),
+    defineField({
       name: "logoUrl",
       title: "Logo URL",
       type: "url",
-    },
-    {
+    }),
+    defineField({
       name: "url",
       title: "URL",
       type: "url",
       initialValue: "https://",
-    },
-    {
+    }),
+    defineField({
       name: "exampleUrl",
       title: "Example URL",
       type: "url",
-    },
-    {
+    }),
+    defineField({
       name: "tags",
       title: "Tags",
       type: "array",
@@ -48,8 +91,8 @@ const IntegrationSchema = {
       options: {
         layout: "tags",
       },
-    },
-    {
+    }),
+    defineField({
       name: "features",
       title: "Features",
       type: "array",
@@ -57,14 +100,14 @@ const IntegrationSchema = {
       options: {
         layout: "list",
       },
-    },
-    {
+    }),
+    defineField({
       name: "sponsored",
       title: "Is Sponsored?",
       type: "boolean",
-      intialValue: false,
-    },
+      initialValue: false,
+    }),
   ],
-}
+});
 
-export default IntegrationSchema
+export default IntegrationSchema;
