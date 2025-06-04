4511bd3c9d506c7d25ee5978ff1b4addfd48cb3a
Ali <ali.hussein.pre@gmail.com>
2025-06-04 11:17:02 +0200

    refactor: add custom metadata to studio

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -12,8 +12,12 @@ import config from "../../../sanity.config"
 
 export const dynamic = "force-static"
 
-export { metadata, viewport } from "next-sanity/studio"
+export { viewport } from "next-sanity/studio"
 
+export const metadata = {
+  title: "Nextradar Studio",
+  description: "Nextradar Studio",
+}
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
